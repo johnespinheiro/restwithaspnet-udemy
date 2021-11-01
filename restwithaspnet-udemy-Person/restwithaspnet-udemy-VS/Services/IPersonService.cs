cc965562ff493e48071a6e386a8ccd651a615a08
johnespinheiro <johnes.armt@gmail.com>
2021-11-01 17:41:55 -0300

    adding models and services

---
+++ restwithaspnet-udemy-Person/restwithaspnet-udemy-VS/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using restwithaspnet_udemy_VS.Model;
+
+namespace restwithaspnet_udemy_VS.Services
+{
+   public interface IPersonService
+    {
+        Person Create(Person Person);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person Person);
+
+        void Delete(long id);
+    }
+}
