c0efd96ef310a6e4ff016daa8ed8561f0c2dbd81
johnespinheiro <johnes.armt@gmail.com>
2021-11-01 17:13:54 -0300

    working with four verbs

---
+++ restwithaspnet-udemy-Person/restwithaspnet-udemy-VS/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restwithaspnet_udemy_VS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]  //primeira parte url - pega o controller ( calculator )
+    public class PersonController : ControllerBase  // nome da rota e controller que o router pega (Calculator)
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{FirstNumber}/{SecondNumber}")]  //segunda parte, onde o 'SUM' o fez encontrar o GET - com os parametros de first e second number
+        public IActionResult Sum(string FirstNumber, string SecondNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+    }
+}
---
+++ restwithaspnet-udemy-Person/restwithaspnet-udemy-VS/Model/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restwithaspnet_udemy_VS.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String Address { get; set; }
+
+        public String Gender { get; set; }
+
+    }
+}
